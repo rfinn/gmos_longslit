5fd7b4a2629a2a6cf4ba2f65c226c4251b6aab09
rfinn <rfinn@siena.edu>
2025-10-03 10:06:21 -0400

    script for reducing data

---
+++ reduce.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from recipe_system import cal_service
+
+caldb = cal_service.set_local_database()
+caldb.init()
+
+
+# You can manually add processed calibrations with caldb.add_cal(<filename>),
+# list the database content with caldb.list_files(), and caldb.remove_cal(<filename>) to remove a file from the database (it will not remove the file on disk.)
+
