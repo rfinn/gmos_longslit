4236725605758152814c1b2ddb63813e31887c03
rfinn <rfinn@siena.edu>
2025-10-03 10:09:39 -0400

    script for reducing data

--- reduce.py
+++ reduce.py
@@ -3,8 +3,11 @@
 from recipe_system import cal_service
 
 caldb = cal_service.set_local_database()
-caldb.init()
 
+try:
+    caldb.init()
+except LocalManagerError:
+    print("cal database already exists")
 
 # You can manually add processed calibrations with caldb.add_cal(<filename>),
 # list the database content with caldb.list_files(), and caldb.remove_cal(<filename>) to remove a file from the database (it will not remove the file on disk.)
